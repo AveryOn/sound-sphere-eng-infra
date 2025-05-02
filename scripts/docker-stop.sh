ca52fd826a57cf833510bf94c9307c1392d27803
AveryOn <lnkonion2@gmail.com>
2025-05-02 09:16:10 +0500

    upd config docker-stop

--- scripts/docker-stop.sh
+++ scripts/docker-stop.sh
@@ -1,4 +1,21 @@
 #!/bin/bash
-docker compose -f docker-compose.local.yaml down
-docker compose -f docker-compose.dev.yaml down
-docker compose -f docker-compose.prod.yaml down
+# Stopped LOCAL
+docker compose \
+  --env-file .env.local \
+  -p local__sound-sphere-eng \
+  -f docker-compose.local.yaml \
+  down
+
+# Stopped DEV
+docker compose \
+  --env-file .env.dev \
+  -p dev__sound-sphere-eng \
+  -f docker-compose.dev.yaml \
+  down
+
+# Stopped PROD
+docker compose \
+  --env-file .env.prod \
+  -p prod__sound-sphere-eng \
+  -f docker-compose.prod.yaml \
+  down
