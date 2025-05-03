56e73fd59f878c961b384d0d38da0aa1c1c05df7
AveryOn <lnkonion2@gmail.com>
2025-05-04 00:34:12 +0500

    upd scripts

--- scripts/docker-local.sh
+++ scripts/docker-local.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
+docker compose -f docker-compose.local.yaml down --volumes --remove-orphans
+docker image prune -a -f
 docker compose \
   --env-file .env.local \
   -p local__sound-sphere-eng \
